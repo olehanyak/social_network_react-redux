7572f67e3acb07fddda98d075563ceed8e4a649d
Oleh Hanyak <ohanyak@gmail.com>
2020-12-31 01:04:57 +0200

    add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,12 +6,17 @@ const Header = (props) => {
   console.log(props);
   return (
     <header className={styles.headerSection}>
-      <div className={styles.sotNet}>moonmen</div>
+      <div className={styles.sotNet}>
+        m<b className={styles.moonLetter}>o</b>onmen
+      </div>
 
       <div className={styles.loginBlock}>
         {props.isAuth ? (
           <div>
-            <span className={styles.userName}>{props.login}</span> <button className="btn btn-outline-primary" onClick={props.logout}>Log out</button>
+            <span className={styles.userName}>{props.login}</span>{" "}
+            <button className="btn btn-outline-primary" onClick={props.logout}>
+              Log out
+            </button>
           </div>
         ) : (
           <NavLink to={"/login"}>Login</NavLink>
